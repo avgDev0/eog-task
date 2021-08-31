acb32ec1595ace1f2d32fc5061de20cb4ffe221c
Luis Mercado <mercadoluisdaniel@gmail.com>
2021-08-31 18:06:25 -0500

    Taking the apollo client all the way up

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  split,
+  HttpLink,
+} from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import store from './redux/store';
 import 'react-toastify/dist/ReactToastify.css';
-// import Header from './components/Header';
 import Wrapper from './components/Wrapper';
-// import NowWhat from './components/NowWhat';
 import MetricsSection from './components/MetricsSection';
 
 const theme = createTheme({
@@ -24,14 +31,45 @@ const theme = createTheme({
   },
 });
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://react.eogresources.com/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({
+  uri: 'https://react.eogresources.com/graphql',
+});
+
+const clientLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+
+    return (
+      definition.kind === 'OperationDefinition'
+      && definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const apolloClient = new ApolloClient({
+  link: clientLink,
+  cache: new InMemoryCache(),
+});
+
 const App = () => (
   <MuiThemeProvider theme={theme}>
     <Provider store={store}>
-      <CssBaseline />
-      <Wrapper>
-        <MetricsSection />
-        <ToastContainer />
-      </Wrapper>
+      <ApolloProvider client={apolloClient}>
+        <CssBaseline />
+        <Wrapper>
+          <MetricsSection />
+          <ToastContainer />
+        </Wrapper>
+      </ApolloProvider>
     </Provider>
   </MuiThemeProvider>
 );
--- src/Features/MetricsSelector/MetricsSelector.tsx
+++ src/Features/MetricsSelector/MetricsSelector.tsx
@@ -1,21 +1,13 @@
-import React, { FC } from 'react';
+import React from 'react';
 import {
-  ApolloClient,
-  ApolloProvider,
   useQuery,
   gql,
-  InMemoryCache,
 } from '@apollo/client';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { Typography } from '@material-ui/core';
 import Chip from '../../components/Chip';
 // import MultiSelector from '../../components/MultiSelect';
 import CheckboxGroup from '../../components/CheckboxGroup';
-
-const client = new ApolloClient({
-  uri: 'https://react.eogresources.com/graphql',
-  cache: new InMemoryCache(),
-});
 
 const query = gql`
   query getMetrics {
@@ -27,7 +19,7 @@ type MetricsDataResponse = {
   getMetrics: string[];
 };
 
-const MetricsSelector: FC = () => {
+export default function MetricsSelector() {
   const { loading, error, data } = useQuery<MetricsDataResponse>(query);
 
   if (loading) return <LinearProgress />;
@@ -37,10 +29,4 @@ const MetricsSelector: FC = () => {
   const { getMetrics: metrics } = data;
 
   return <CheckboxGroup options={metrics} />;
-};
-
-export default () => (
-  <ApolloProvider client={client}>
-    <MetricsSelector />
-  </ApolloProvider>
-);
+}
